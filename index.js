88e78abd3e038cb7e0d8d4a570fb7282d5b042ba
Nazmul19285 <nazmulhossain19285@gmail.com>
2022-05-24 18:13:14 +0600

    working on to get a product by id

---
+++ index.js
@@ -0,0 +1,46 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mepvj.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        await client.connect();
+        const productsCollection = client.db("pedaler").collection("products");
+        // get all products
+        app.get('/products', async(req, res) => {
+            const query = {};
+            const cursor = productsCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        });
+        // get a product with id
+        app.get('/products/:id', async(req, res) => {
+          const id = req.params.id;
+          console.log(id);
+          res.send(id);
+        });
+
+    }
+    finally{
+
+    }
+
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Hello from pedaler')
+})
+
+app.listen(port, () => {
+  console.log(`Pedaler app listening on port ${port}`)
+})
