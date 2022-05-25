47f5b3aa5e4fbed14c0211cce86aa4f6bbc7eea7
Nazmul19285 <nazmulhossain19285@gmail.com>
2022-05-25 14:58:49 +0600

    a little change

--- index.js
+++ index.js
@@ -50,7 +50,6 @@ async function run() {
     // Delete an order
     app.delete('/orders/:id', async (req, res) => {
       const id = req.params.id;
-      console.log(id);
       const query = { _id: ObjectId(id) };
       const result = await ordersCollection.deleteOne(query);
       res.send(result);
